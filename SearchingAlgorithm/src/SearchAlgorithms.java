a31cbd259079c8eac1cbb94bb850e066ca03db57
Abdelrahman Ali <abdelrahmanalisalem@gmail.com>
2023-11-07 23:00:50 +0200

    starting code

---
+++ SearchingAlgorithm/src/SearchAlgorithms.java
@@ -0,0 +1,27 @@
+public class SearchAlgorithms {
+
+    static void breadthFirstSearch(Node initialNode) {
+
+    }
+
+    static void depthFirstSearch(Node initialNode) {
+
+    }
+
+    static void iterativeDeepeningSearch(Node initialNode) {
+
+    }
+
+    static void uniformCostSearch(Node initialNode) {
+
+    }
+
+    static void greedySearch(Node initialNode, int heuristic) {
+
+    }
+
+    static void aStarSearch(Node initialNode, int heuristic) {
+
+    }
+
+}
