d4b51ee1d0b31afe96a7185b2ef83934d744439d
Abdelrahman Ali <abdelrahmanalisalem@gmail.com>
2023-11-07 22:13:17 +0200

    Create SearchAlgorithms.java

---
+++ SearchAlgorithms.java
@@ -0,0 +1,25 @@
+public class SearchAlgorithms {
+    static void breadthFirstSearch(Node initialState) {
+
+    }
+
+    static void depthFirstSearch(Node initialState) {
+
+    }
+
+    static void iterativeDeepeningSearch(Node initialState) {
+
+    }
+
+    static void uniformCostSearch(Node initialState) {
+
+    }
+
+    static void greedySearch(Node initialState, int heuristic) {
+
+    }
+
+    static void aStarSearch(Node initialState, int heuristic) {
+
+    }
+}
