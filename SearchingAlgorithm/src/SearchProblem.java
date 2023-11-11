1015eae7181743e764bfed8a917effad0130049e
alighieth <alighieth2709@gmail.com>
2023-11-11 21:43:58 +0200

    Update SearchProblem.java

--- SearchingAlgorithm/src/SearchProblem.java
+++ SearchingAlgorithm/src/SearchProblem.java
@@ -294,10 +294,20 @@ public class SearchProblem {
         // During delay of any request -> Build1, Build2, Wait
         // waiting only allowed if a delivery is taking place
 
-        if (this.deliveries == null || currentNode.getOperator().equals(null)) {
+        if(currentNode.getOperator() == null) {
+            possibleOperators.addFirst(SearchProblem.Operators.WAIT);
+            possibleOperators.addFirst(SearchProblem.Operators.REQUEST_ENERGY);
+            possibleOperators.addFirst(SearchProblem.Operators.REQUEST_FOOD);
+            possibleOperators.addFirst(SearchProblem.Operators.REQUEST_MATERIALS);
+            possibleOperators.addFirst(SearchProblem.Operators.Build1);
+            possibleOperators.addFirst(SearchProblem.Operators.Build2);
+            return possibleOperators;
+        }
+
+        if (this.deliveries == null ) {
             possibleOperators.addFirst(SearchProblem.Operators.WAIT);
         } 
-        if(this.deliveries instanceof RequestDelivery || currentNode.getOperator().equals(null)) {
+        else {
             possibleOperators.addFirst(SearchProblem.Operators.REQUEST_ENERGY);
             possibleOperators.addFirst(SearchProblem.Operators.REQUEST_FOOD);
             possibleOperators.addFirst(SearchProblem.Operators.REQUEST_MATERIALS);
